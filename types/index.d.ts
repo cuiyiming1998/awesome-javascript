a1928e89a7351ab970fc8715397e45d64e000009
cuiyiming1998 <1269567664@qq.com>
2022-11-12 22:39:13 +0800

    🎨 feat(global): 添加全局ts类型

---
+++ types/index.d.ts
@@ -0,0 +1,31 @@
+declare type Recordable<T = any> = Record<string, T>
+
+declare interface Fn<T = any, R = T> {
+	(...arg: T[]): R
+}
+
+declare interface PromiseFn<T = any, R = T> {
+	(...arg: T[]): Promise<R>
+}
+
+declare type ValueOf<T> = T[keyof T]
+
+declare type Nullable<T> = T | null
+
+declare type NonNullable<T> = T extends null | undefined ? never : T
+
+declare type ReadonlyRecordable<T = any> = {
+	readonly [key: string]: T
+}
+
+declare type Indexable<T = any> = {
+	[key: string]: T
+}
+
+declare type DeepPartial<T> = {
+	[P in keyof T]?: DeepPartial<T[P]>
+}
+
+declare type TimeoutHandle = ReturnType<typeof setTimeout>
+
+declare type IntervalHandle = ReturnType<typeof setInterval>
