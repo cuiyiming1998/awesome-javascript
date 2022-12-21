5c7fb978f17fdb296382ef9c994e53a884f5121e
cuiyiming1998 <1269567664@qq.com>
2022-12-21 16:28:53 +0800

    ✏️ fix: 修复拼写错误

--- pipe/__test__/pipe.test.ts
+++ pipe/__test__/pipe.test.ts
@@ -1,13 +1,13 @@
 import { describe, it, expect } from "vitest"
 import { pipe } from ".."
 
-describe('deepClone', () => {
+describe('pipe', () => {
   it('happy path', () => {
     const add4 = num => num +4
-    const mutiply3 = num => num * 3
+    const multiply3 = num => num * 3
     const divide2 = num => num / 2
 
-    const computed = pipe(add4, mutiply3, divide2)
+    const computed = pipe(add4, multiply3, divide2)
     const res = computed(10)
     expect(res).toBe(21)
   })
